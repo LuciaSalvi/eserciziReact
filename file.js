aa13deb22072fbceebe1fa14637db6a64e90da66
Lucia Salvi <luciasalvi82@gmail.com>
2022-07-12 12:02:01 +0200

    nuovo aggiornamento

--- file.js
+++ file.js
@@ -13,6 +13,7 @@ var HelloWorld = () => {
 }
 */
 
+/*
 const testoRosso = {color:"red"}
 
 var HelloWorld = () => {
@@ -26,5 +27,65 @@ var HelloWorld = () => {
         </div>
     )
 }
-
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+*/
+
+/*
+class HelloWorld extends React.Component{
+    render(){
+        return (
+        <div>
+            <p className = {this.props.config}>Ciao pianeta terra.</p>
+            <p className="diversa">Come procede l'estate?</p>
+        </div>
+        )
+    }
+}
+ReactDOM.render(<HelloWorld config = "Pippo" />, document.getElementById("root"));
+*/
+/*
+class HelloWorld extends React.Component{
+    render(){
+        return (
+        <div>
+            <p className = {this.props.config.prima}>Ciao pianeta terra.</p>
+            <p className = {this.props.config.seconda}>Come procede l'estate?</p>
+        </div>
+        )
+    }
+}
+
+//ReactDOM.render(<HelloWorld config = {{prima:"ciao", seconda:"pippo"}} />, document.getElementById("root"));
+*/
+/*
+class HelloWorld extends React.Component{
+    render(){
+        return (
+        <div>
+            <p className = {this.props.config.prima}>Ciao pianeta terra.</p>
+            <p className = {this.props.config.seconda}>Come procede l'estate?</p>
+            <p>Un attimo {this.props.carota}</p>
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(<HelloWorld config = {{prima:"ciao", seconda:"pippo"}} carota = {'genoveffa'}/>, document.getElementById("root"));
+*/
+const configurazione = {
+    prima: 'ciao',
+    seconda: 'pippo'
+}
+class HelloWorld extends React.Component{
+    render(){
+        return (
+        <div>
+            <p className = {this.props.config.prima}>Ciao pianeta terra.</p>
+            <p className = {this.props.config.seconda}>Come procede l'estate?</p>
+            <p>Un attimo {this.props.carota}</p>
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(<HelloWorld config = {configurazione} carota = {'genoveffa'}/>, document.getElementById("root"));
